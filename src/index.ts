0bd4fd5776e83533021ec506cd0e89f6fabf28fc
Jason Dekarske <dekarskej@gmail.com>
2021-06-03 21:34:39 -0700

    needed to downgrade webpack https://github.com/RobotWebTools/roslibjs/issues/382

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./static/style.css"
 import { CubeContainer } from "./cubecontainer"
-// import { ROSInterface } from "./ros";
+import { ROSInterface } from "./ros";
 
 let startContainer = new CubeContainer("startcontainer", 2, 8);
 let instructionContainer = new CubeContainer("instructioncontainer", 2, 4);
@@ -10,4 +10,4 @@ startContainer.fillContainer();
 instructionContainer.fillContainer();
 commandContainer.fillContainer(true);
 
-// let ros = new ROSInterface();
+let ros = new ROSInterface();
