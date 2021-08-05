d8b3bef97a1f4ad39d181149a9ba381284c368c6
Jason Dekarske <dekarskej@gmail.com>
2021-08-04 17:56:31 -0700

    reset experiment button

--- src/index.ts
+++ src/index.ts
@@ -9,25 +9,38 @@ let commandContainer = new CubeContainer("commandcontainer", 2, 4);
 
 // Setup the experiment
 //---------------------
-commandContainer.fillContainer(true); // empty
-instructionContainer.fillContainer(true); //empty
 
-// generate random cubes
-startContainer.fillContainer();
+function setupExperiment() {
 
-// grab four random ones
-var start_cubes = startContainer.listCubes();
-var random_start_cubes_i = shuffle(range(0, start_cubes.cubelist.length)); // randomly selected cubes
+    // make sure the containers are empty
+    commandContainer.clearContainer()
+    instructionContainer.clearContainer()
+    startContainer.clearContainer()
 
-// put those in four random instruction places
-var random_instruction_cubes_i = shuffle(range(0, instructionContainer.numberofshapes));
-for (let i = 0; i < 4; i++) {
-    instructionContainer.setcubeColor(
-        random_instruction_cubes_i[i], // the position in the instruction box
-        start_cubes.cubelist[random_start_cubes_i[i]].color) // random color from start box
+    commandContainer.fillContainer(true); // empty
+    instructionContainer.fillContainer(true); //empty
+
+    // generate random cubes
+    startContainer.fillContainer();
+
+    // grab four random ones
+    var start_cubes = startContainer.listCubes();
+    var random_start_cubes_i = shuffle(range(0, start_cubes.cubelist.length)); // randomly selected cubes
+
+    // put those in four random instruction places
+    var random_instruction_cubes_i = shuffle(range(0, instructionContainer.numberofshapes));
+    for (let i = 0; i < 4; i++) {
+        instructionContainer.setcubeColor(
+            random_instruction_cubes_i[i], // the position in the instruction box
+            start_cubes.cubelist[random_start_cubes_i[i]].color) // random color from start box
+    }
 }
 
+setupExperiment();
 let ros = new ROSInterface();
+
+// Setup the buttons
+//---------------------
 
 document.getElementById("send_btn").onclick = (() => {
     ros.deleteAllCubes();
@@ -56,4 +69,8 @@ document.getElementById("sort_btn").onclick = (() => {
     } else {
         return false;
     }
+})
+
+document.getElementById("replace_btn").onclick = (() => {
+    setupExperiment();
 })
