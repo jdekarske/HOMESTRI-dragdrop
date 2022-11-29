75770f24956142b8c9344c6a243c926d166dc383
Jason Dekarske <dekarskej@gmail.com>
2022-11-28 22:50:31 -0800

    fixed survey

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,57 +1,72 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../static/style.css';
+import $ from 'jquery';
 
-function advance() {
+const subject = {
+  age: null,
+  genderIdentity: null,
+  addGender: null,
+  sex: null,
+  ethnicity: null,
+  aian: 'false',
+  asian: 'false',
+  baa: 'false',
+  nhopi: 'false',
+  white: 'false',
+  colorblind: null,
+  make: null,
+  videoGames: null,
+  yesGames: null,
+  email: null,
+  surveytime: null,
+};
+
+interface FormObject {
+  name: string,
+  value: string,
+}
+
+// Function used to check if all questions were filled in info form, if so, starts the experiment
+document.getElementById('checkInfo').addEventListener('click', () => {
+  const values: FormObject[] = $('#infoform').serializeArray();
+  console.log(values);
+  values.forEach((obj) => {
+    subject[obj.name] = obj.value;
+  });
+
+  subject.surveytime = Date.now();
+
+  // don't accept if they don't fill out survey
+  if (
+    ![
+      subject.age,
+      subject.ethnicity,
+      subject.genderIdentity,
+      subject.sex,
+      subject.colorblind,
+      subject.make,
+      subject.videoGames,
+      subject.yesGames,
+    ].every((e) => e)
+  ) {
+    alert('incomplete');
+    return false;
+  }
+
+  jatos.addJatosIds(subject);
+  jatos.startNextComponent(subject);
+});
+
+const advancebtn = document.getElementById('advance');
+advancebtn?.addEventListener('click', () => {
   jatos.onLoad(() => {
     jatos.startNextComponent();
   });
-}
+});
 
-function abort() {
+const abortbtn = document.getElementById('abort');
+abortbtn?.addEventListener('click', () => {
   jatos.onLoad(() => {
     jatos.abortStudy('Subject elected to not continue.');
   });
-}
-
-// const subject = {
-//   age: null,
-//   genderIdentity: null,
-//   addGender: null,
-//   sex: null,
-//   ethnicity: null,
-//   aian: 'false',
-//   asian: 'false',
-//   baa: 'false',
-//   nhopi: 'false',
-//   white: 'false',
-//   colorblind: null,
-//   make: null,
-//   videoGames: null,
-//   yesGames: null,
-//   email: null,
-//   surveytime: null,
-// };
-
-// // Function used to check if all questions were filled in info form, if so, starts the experiment
-// function checkInfo() {
-//   const values = $('#infoform').serializeArray();
-//   console.log(values);
-//   $(values).each((index, obj) => {
-//     subject[obj.name] = obj.value;
-//   });
-
-//   subject.surveytime = Date.now();
-
-//   // don't accept if they don't fill out survey
-//   if (![subject.age, subject.ethnicity, subject.genderIdentity, subject.sex, subject.colorblind, subject.make, subject.videoGames, subject.yesGames].every((e) => e)) {
-//     alert('incomplete');
-//     return false;
-//   }
-
-//   }
-//     jatos.showBeforeUnloadWarning(true);
-
-//     jatos.addJatosIds(subject); // adds the component information to the results
-//     jatos.startNextComponent(subject); // submit data and move on to grid or ctt
-
-//   return true;
+});
