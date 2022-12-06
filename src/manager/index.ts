4ce31350c33d2d2951c8513773c9201aec3d95b4
Jason Dekarske <dekarskej@gmail.com>
2022-12-06 09:11:24 -0800

    use fetch instead

--- src/manager/index.ts
+++ src/manager/index.ts
@@ -1,22 +1,16 @@
 import '../static/style.css';
 
+const endpoint = 'https://***REMOVED***/manager/';
+
 jatos.onLoad(() => {
-  function httpGetAsync(theUrl: string, callback?: (response: string) => void) {
-    const xmlHttpReq = new XMLHttpRequest();
-    xmlHttpReq.onreadystatechange = () => {
-      if (xmlHttpReq.readyState === 4 && xmlHttpReq.status === 200) {
-        callback(xmlHttpReq.responseText);
-      }
-      xmlHttpReq.open('GET', theUrl, true); // true for asynchronous
-      xmlHttpReq.send(null);
-    };
-  }
-
   if ('simulation' in jatos.componentJsonInput) {
-    httpGetAsync(`https://***REMOVED***/manager/${jatos.componentJsonInput.simulation}/1`, (result) => {
-            alert(result);
-    });
+    const path = `${jatos.componentJsonInput.simulation}/1`;
+    fetch(endpoint + path)
+      .then(
+        (response) => {
+          console.log(response);
+          jatos.startNextComponent();
+        },
+      );
   }
-
-  jatos.startNextComponent();
 });
