0ca4d58df328d578c5b456030ab8d305284ec8c1
Jason Dekarske <dekarskej@gmail.com>
2022-12-13 21:58:31 -0800

    demo survey compliance

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,8 @@ import '../static/style.css';
 // a generalized script to handle most forms
 // There can only be one form element per page. All required inputs must have an "required"
 // attribute as in: <input required type="radio" name="something" value="i" />
+// input has name
+// div has id
 
 function intersects(a: any[], b: any[]): boolean {
 // check if elements exist in both a and b
@@ -15,28 +17,30 @@ function intersects(a: any[], b: any[]): boolean {
 // check if required fields are set
 document.getElementById('checkInfo')?.addEventListener('click', () => {
   const form = document.getElementsByTagName('form')[0];
-  const requiredKeys = Array.from(form.getElementsByTagName('input'))
-    .filter((x) => (x as HTMLInputElement).required)
-    .map((x) => x.name)
+  const requiredKeys = Array.from(form.querySelectorAll('[required]'))
+    .map((x) => x.getAttribute('name'))
     .filter((value, index, self) => self.indexOf(value) === index); // unique values
 
   let data: Object[] = [];
   const values = new FormData(form);
   values.forEach((key, value) => {
-    if (typeof key === 'string') {
+    if (key && typeof key === 'string') {
       const newField: Object = {};
       newField[value] = key; // warning, this is counterintuitive
       data.push(newField);
     }
   });
+  data.push({ surveyTime: Date.now() });
   data = Object.assign({}, ...data);
+
+  console.log(requiredKeys);
+  console.log(data);
 
   // don't accept if they don't fill out survey
   if (!intersects(requiredKeys, Object.keys(data))) {
     alert('Please complete all necessary fields');
   } else {
     jatos.onLoad(() => {
-      data.push({ surveyTime: Date.now() });
       jatos.addJatosIds(data);
       jatos.startNextComponent(data);
     });
