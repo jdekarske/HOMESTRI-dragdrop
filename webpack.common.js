673518289c96a198b9a437a2e120a84a2555bddb
Jason Dekarske <dekarskej@gmail.com>
2023-03-02 11:44:40 -0800

    add protocol to config

--- webpack.common.js
+++ webpack.common.js
@@ -53,6 +53,7 @@ module.exports = {
         new DefinePlugin({
             'TEST': "'test!!!'",
             'process.env.host': JSON.stringify(config["host"]) || null,
+            'process.env.protocol': JSON.stringify(config["protocol"]) || null,
             'process.env.endpoints_manager': JSON.stringify(config["endpoints"]["manager"]) || null,
             'process.env.endpoints_simulation': JSON.stringify(config["endpoints"]["simulation"]) || null,
         }),
